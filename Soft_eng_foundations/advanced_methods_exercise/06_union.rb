41588e92aa2724bd2b816468e19e87d936bb42dc
codeloopy <codeloopy@gmail.com>
2019-08-02 22:56:50 -0400

    06 completed

--- Soft_eng_foundations/advanced_methods_exercise/06_union.rb
+++ Soft_eng_foundations/advanced_methods_exercise/06_union.rb
@@ -2,5 +2,11 @@
 # The method should return an array containing all elements of the given arrays.
 
 
+def union(*arrays)
+  merged_array = arrays.flatten
+  return merged_array
+end
+
+
 p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
 p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
