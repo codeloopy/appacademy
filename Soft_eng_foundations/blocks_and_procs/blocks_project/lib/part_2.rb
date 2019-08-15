5df2dd41e6736c643a2e99c2b369bd771a044413
codeloopy <codeloopy@gmail.com>
2019-08-15 17:57:09 -0400

    completed part 2

---
+++ Soft_eng_foundations/blocks_and_procs/blocks_project/lib/part_2.rb
@@ -0,0 +1,24 @@
+require 'byebug'
+
+def all_words_capitalized?(words)
+  words.all? { |w| w == w.capitalize }
+end
+
+
+
+def no_valid_url?(urls)
+  valid_domains = ['com', 'net', 'io', 'org']
+  urls.none? do |web_addr|
+    domain = web_addr.split('.')[-1]
+    valid_domains.include?(domain)
+  end
+end
+
+
+
+def any_passing_students?(students)
+  students.any? do |student|
+    avg_grade = (student[:grades].sum / student[:grades].length)
+     avg_grade >= 75
+  end
+end
