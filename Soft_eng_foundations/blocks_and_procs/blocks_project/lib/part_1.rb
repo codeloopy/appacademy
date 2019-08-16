d6dded4536e63dc27b65074de600254f59c1b648
codeloopy <codeloopy@gmail.com>
2019-08-15 20:41:57 -0400

    aAs solutions

--- Soft_eng_foundations/blocks_and_procs/blocks_project/lib/part_1.rb
+++ Soft_eng_foundations/blocks_and_procs/blocks_project/lib/part_1.rb
@@ -42,3 +42,49 @@ end
 def aba_array(words_array)
   words_array.map { |word| aba_translate(word) }
 end
+
+
+
+
+
+
+
+
+
+
+# # aA's Solution
+
+# def select_even_nums(nums)
+#   nums.select(&:even?)
+# end
+
+
+# def reject_puppies(dogs)
+#   dogs.reject { |dog| dog["age"] <= 2 }
+# end
+
+
+# def count_positive_subarrays(arrays)
+#   arrays.count { |subarr| subarr.sum > 0  }
+# end
+
+
+# def aba_translate(word)
+#   vowels = 'aeiou'
+#   new_word = ''
+
+#   word.each_char do |char|
+#     if vowels.include?(char)
+#       new_word += char + "b" + char
+#     else
+#       new_word += char
+#     end
+#   end
+
+#   new_word
+# end
+
+
+# def aba_array(words_array)
+#   words_array.map { |word| aba_translate(word) }
+# end
--- Soft_eng_foundations/blocks_and_procs/blocks_project/lib/part_2.rb
+++ Soft_eng_foundations/blocks_and_procs/blocks_project/lib/part_2.rb
@@ -6,6 +6,7 @@ end
 
 
 
+
 def no_valid_url?(urls)
   valid_domains = ['com', 'net', 'io', 'org']
   urls.none? do |web_addr|
@@ -16,9 +17,42 @@ end
 
 
 
+
 def any_passing_students?(students)
   students.any? do |student|
     avg_grade = (student[:grades].sum / student[:grades].length)
      avg_grade >= 75
+  end
+end
+
+
+
+
+
+
+
+
+
+# aA's Solution
+
+def all_words_capitalized?(words)
+  words.all? { |w| w == w.capitalize }
+end
+
+
+def no_valid_url?(urls)
+  valid_endings = ['com', 'net', 'io', 'org']
+  
+  urls.none? do |url|
+    valid_endings.any? { |ending| url.end_with?(ending)}
+  end
+end
+
+
+def any_passing_students?(students)
+  students.any? do |student|
+    grades = student[:grades]
+    avg = grades.sum / grades.length * 1.0
+    avg >= 75
   end
 end
