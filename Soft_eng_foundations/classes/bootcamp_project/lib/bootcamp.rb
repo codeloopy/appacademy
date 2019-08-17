1d8939a4eab9dca882115f9df26afa2d1e1df3ca
codeloopy <codeloopy@gmail.com>
2019-08-17 00:07:28 -0400

    Classes exercise 2 completed

---
+++ Soft_eng_foundations/classes/bootcamp_project/lib/bootcamp.rb
@@ -0,0 +1,68 @@
+class Bootcamp
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new { |name, grade| name[grade] = [] }
+  end
+
+  def name
+    @name
+  end
+
+  def slogan
+    @slogan
+  end
+
+  def teachers
+    @teachers
+  end
+
+  def students
+    @students
+  end
+
+  def hire(teacher)
+    @teachers << teacher
+  end
+
+  def enroll(student)
+    if @students.length < @student_capacity
+      @students << student
+      return true
+    else
+      return false
+    end
+  end
+
+  def enrolled?(student)
+    @students.include?(student)
+  end
+
+  def student_to_teacher_ratio
+    @students.count / @teachers.count
+  end
+
+  def add_grade(student, grade)
+    if @students.include?(student)
+      @grades[student] << grade
+      true
+    else
+      false
+    end
+  end
+
+  def num_grades(student)
+    @grades[student].length
+  end
+
+  def average_grade(student)
+    if @grades[student].empty?
+      return nil
+    else
+      @grades[student].sum / @grades[student].length * 1.0
+    end
+  end
+end
