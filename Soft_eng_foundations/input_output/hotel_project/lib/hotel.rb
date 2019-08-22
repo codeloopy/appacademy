41f24b7ecb4231bb1771386ff1e5df57e5d9595a
codeloopy <codeloopy@gmail.com>
2019-08-22 10:55:01 -0400

    Room class done, starting on the hotel class

--- Soft_eng_foundations/input_output/hotel_project/lib/hotel.rb
+++ Soft_eng_foundations/input_output/hotel_project/lib/hotel.rb
@@ -1,5 +1,8 @@
 require_relative "room"
 
 class Hotel
+  def initialize()
+
+  end
   
 end
--- Soft_eng_foundations/input_output/hotel_project/lib/room.rb
+++ Soft_eng_foundations/input_output/hotel_project/lib/room.rb
@@ -1,3 +1,31 @@
 class Room
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
 
+  def capacity
+    @capacity
+  end
+
+  def occupants
+    @occupants
+  end
+
+  def full?
+    @occupants.count == @capacity
+  end
+
+  def available_space
+    @capacity - @occupants.count
+  end
+
+  def add_occupant(person_name)
+    if !self.full?
+      @occupants << person_name
+      true
+    else
+      false
+    end
+  end
 end
