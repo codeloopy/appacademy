0a3fc8940ae85dc3f55809fc22a242585ad0b1d5
codeloopy <codeloopy@gmail.com>
2019-08-23 14:35:00 -0400

    aA's solution

--- Soft_eng_foundations/input_output/hotel_project/lib/hotel.rb
+++ Soft_eng_foundations/input_output/hotel_project/lib/hotel.rb
@@ -8,13 +8,13 @@ class Hotel
     @rooms = Hash.new
     
     room.each do |k,v|
-      self.rooms[k] = Room.new(v)
+      self.rooms[k] = Room.new(v) # <--- aA's solution: @rooms[k] = Room.new(v)
     end
   end
 
 
   def name
-    @name.split(' ').map { |word| word.capitalize}.join(' ')
+    @name.split(' ').map { |word| word.capitalize}.join(' ')  # <--- aA's solution: @name.split(' ').map(&:capitalize).join(' ') -Super elegant
   end
 
 
@@ -24,7 +24,7 @@ class Hotel
 
 
   def room_exists?(room_name)
-    self.rooms.include?(room_name)
+    self.rooms.include?(room_name)   # <--- aA's solution: @rooms.has_key?(room_name)
   end
 
 
@@ -41,6 +41,22 @@ class Hotel
     end
   end
 
+
+  # aA's solution:
+
+  # def check_in(person_name, room_name)
+  #   if self.room_exists?(room_name)
+  #     if @rooms[room_name].add_occupant(person_name)
+  #       puts "check it successful"
+  #     else
+  #       puts "sorry, room is full"
+  #     end
+  #   else
+  #     puts "sorry, room does not exist"
+  #   end
+  # end
+  
+
   # debugger
   def has_vacancy?
     self.rooms.each do |room, capacity|
@@ -53,8 +69,20 @@ class Hotel
   end
 
 
+  # aA's solution --- OMG super elegant!
+
+  # def has_vacancy?
+  #   @rooms.values.any? { |room| room.available_space > 0 }
+  # end
+
+
   def list_rooms
     @rooms.each { |room, c| puts "#{room} #{self.rooms[room].available_space}" }
   end
+
+  # aA's solution
+  # def list_rooms
+  #   @rooms.each { |room_name, room| puts "#{room_name} #{room.available_space}" }
+  # end
 
 end
