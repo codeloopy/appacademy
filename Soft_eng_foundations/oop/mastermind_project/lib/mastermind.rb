332a0c8ef135f65ab33e25c25caabb833dc4e32a
codeloopy <codeloopy@gmail.com>
2019-09-03 22:44:16 -0400

    mastermind completed

--- Soft_eng_foundations/oop/mastermind_project/lib/mastermind.rb
+++ Soft_eng_foundations/oop/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,26 @@
+require 'byebug'
 require_relative "code"
 
 class Mastermind
+
+  def initialize(length)
+    @secret_code = Code.random(length)
+  end
+
+
+  def print_matches(code)    
+    exact = Code.new(@secret_code.pegs).num_exact_matches(code)
+    near = Code.new(@secret_code.pegs).num_near_matches(code)
+    puts "Exact matches: #{exact}"
+    puts "Near matches: #{near}"
+  end
+
+
+  def ask_user_for_guess
+    puts "Enter a code"
+    code = gets.chomp.upcase
+    self.print_matches(Code.from_string(code))
+    code.split('') == @secret_code.pegs
+  end
 
 end
