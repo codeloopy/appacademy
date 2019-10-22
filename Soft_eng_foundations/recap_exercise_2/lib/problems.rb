4974cc2bb971b228c5c05127d32fbbcf634b8df4
codeloopy <codeloopy@gmail.com>
2019-10-21 20:52:07 -0400

    done - needed help from explanation. REVISIT at a later time

--- Soft_eng_foundations/recap_exercise_2/lib/problems.rb
+++ Soft_eng_foundations/recap_exercise_2/lib/problems.rb
@@ -1,29 +1,49 @@
-# Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple
+# Write a method, least_common_multiple, that takes in two numbers and returns
+# the smallest number that is a mutiple
 # of both of the given numbers
 def least_common_multiple(num_1, num_2)
-
+	num_1.lcm(num_2)
 end
 
-
-# Write a method, most_frequent_bigram, that takes in a string and returns the two adjacent letters that appear the
+# Write a method, most_frequent_bigram, that takes in a string and returns
+# the two adjacent letters that appear the
 # most in the string.
 def most_frequent_bigram(str)
+	count = Hash.new(0)
+	(0...str.length-1).each do |i|
+		bigram = str[i..i+1]
+		count[bigram] += 1
+	end
 
+	sorted = count.sort_by { |k,v| v }
+	sorted[-1][0]
+	# sorted.last[0]
 end
 
 
 class Hash
-    # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
-    def inverse
-
+    # Write a method, Hash#inverse, that returns a new hash where
+		#the key-value pairs are swapped
+		
+		def inverse
+			new_hash = {}
+			self.each { |k,v| new_hash[v] = k }
+			new_hash
     end
 end
 
 
 class Array
-    # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
-    def pair_sum_count(num)
-
+    # Write a method, Array#pair_sum_count, that takes in a target number
+    #returns the number of pairs of elements that sum to the given target
+		def pair_sum_count(num)
+			count = 0
+			(0...self.length).each do |idx1|
+				(idx1 + 1...self.length).each do |idx2|
+					count += 1 if self[idx1] + self[idx2] == num
+				end
+			end
+			count
     end
 
     # Write a method, Array#bubble_sort, that takes in an optional proc argument.
@@ -39,7 +59,37 @@ class Array
     # it does not matter which element goes first (i.e. do nothing).
     #
     # This should remind you of the spaceship operator! Convenient :)
-    def bubble_sort(&prc)
+		def bubble_sort(&prc)
+			prc ||= Proc.new { |a, b| a <=> b }
+			
+			sorted = false
 
+			while !sorted
+				sorted = true
+
+				(0...self.length - 1).each do |i|
+					if prc.call(self[i], self[i + 1]) == 1
+						sorted = false
+						self[i], self[i + 1] = self[i + 1], self[i]
+					end
+				end
+			end
+			self
     end
 end
+
+
+
+
+
+
+
+
+
+# Aa's Solution
+
+# def least_common_multiple(num_1, num_2)
+# 	(1..num_1 * num_2).each do |i|
+# 		return i if num_1 == 0 && num_2 == 0
+# 	end
+# end
