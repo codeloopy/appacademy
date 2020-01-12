1007f2a913413cd85c3b64eedfe165edaf4403d7
codeloopy <codeloopy@gmail.com>
2020-01-11 20:45:16 -0500

    my_join done

--- Ruby/enumerables.rb
+++ Ruby/enumerables.rb
@@ -102,6 +102,19 @@ class Array
     final_arr
   end
 
+
+  def my_join(sym = "")
+    i = 0
+    new_word = ''
+
+    while i < self.length - 1
+      new_word += "#{self[i]}#{sym}"
+      i += 1      
+    end
+
+    new_word + self[-1]
+  end
+
 end
 
 
@@ -109,38 +122,6 @@ end
 
 
 =begin
-My Zip
-Write my_zip to take any number of arguments. It should return a new array
-containing self.length elements. Each element of the new array should be
-an array with a length of the input arguments + 1 and contain the merged elements
-at that index. If the size of any argument is less than self,
-nil is returned for that location.
-
-Example:
-
-a = [ 4, 5, 6 ]
-b = [ 7, 8, 9 ]
-[1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
-a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
-[1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
-
-c = [10, 11, 12]
-d = [13, 14, 15]
-[1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
-My Rotate
-Write a method my_rotate that returns a new array containing all the elements
-of the original array in a rotated order. By default, the array should rotate
-by one element. If a negative value is given, the array is rotated
-in the opposite direction.
-
-Example:
-
-a = [ "a", "b", "c", "d" ]
-a.my_rotate         #=> ["b", "c", "d", "a"]
-a.my_rotate(2)      #=> ["c", "d", "a", "b"]
-a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
-a.my_rotate(15)     #=> ["d", "a", "b", "c"]
-
 My Join
 my_join returns a single string containing all the elements of the array,
 separated by the given string separator. If no separator is given,
