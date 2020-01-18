ab58dd93726421d881e5ad59761d5a8538d35ce9
codeloopy <codeloopy@gmail.com>
2020-01-17 20:32:59 -0500

    setup the game class

--- Ruby/ghost.rb
+++ Ruby/ghost.rb
@@ -11,6 +11,22 @@ class Game
 
   def play_round
   end
+
+  def current_player
+  end
+
+  def previous_player
+  end
+
+  # updates current_player and previous_player
+  def next_player!
+  end
+
+  def take_turn(player)
+  end
+
+  def valid_play?(string)
+  end
 end
 
 new_game = Game.new('Pedro', 'Sandra')
